0ce2f6c4a79192ed2deb4e49b9ed2ef93e07d5c0
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2023-02-14 22:05:50 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,41 @@
+# print(6)
+# print(6.8)
+# print(6)
+# print(6)
+
+'''variables in python 
+rules of a variable '''
+""" Variables rules we use a method
+ called identifier"""
+
+
+# print('''variables in python 
+#          rules of a variable ''')
+# print("""Variables rules we use a method
+#  called isidentifier""")
+
+# a="python program"
+# print(type(a))
+
+# a=5
+# b=8
+# c=3.7
+# d=False
+# e=True
+# print(e)
+
+# course='python'
+# print('we are learning',course)
+
+# print('a'.isidentifier())
+# print('A'.isidentifier())
+# print('_'.isidentifier())
+# print('3a'.isidentifier())
+# print('a v'.isidentifier())
+# print('is'.isidentifier())
+
+
+# import keyword as kk
+# print(kk.kwlist)
+# print(len(kk.kwlist))
+# print(kk.iskeyword('print'))
